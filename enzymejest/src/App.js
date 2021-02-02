f09c3a8cf5b38ea05f89e521d9dcaf267797ecc4
540792740 <peterwangjiawei@gmail.com>
2021-02-02 15:12:43 -0500

    new update

--- enzymejest/src/App.js
+++ enzymejest/src/App.js
@@ -1,65 +1,15 @@
-// import Header from "./components/Header/Header";
-// import './app.scss';
-import Search from "./components/Search";
-import Searchbutton from "./components/Searchbutton";
-import Searchtable from "./components/Searchtable";
+import Header from "./components/Header/Header";
+import './app.scss';
+import CallTable from "./components/APICallTable/CallTable"
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      title : 'title',
-      content : "content",
-      getData: false,
-      inputValue: "qui est esse"
-    }
-  }
-
-  SearchItemMethod(){
-    if(this.state.getData){
-      let value = this.state.inputValue;
-      value = value.split(" ");
-      value = value.join("%20")
-      console.log("value",value)
-      const apiUrl = 'http://jsonplaceholder.typicode.com/posts?title=' + this.state.inputValue;
-      console.log(apiUrl)
-      
-      fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("ssss",data)
-          this.setState({
-            title:data[0].title,
-            content:data[0].body,
-            getData: false
-          })
-        })
-      }
-    }
-
-  inputChange(value){
-    this.setState({
-        inputValue:value
-    })
-  }
-
-  buttonClick(){
-    this.setState({
-      getData:true
-    })
-    this.SearchItemMethod();
-  }
-
+  
   render() {
     return (
       <div style={{textAlign:"center"}}>
-        {/* <Header/> */}
-        <Search inputChange={(e)=>this.inputChange(e)}></Search>
-        <Searchbutton buttonClick={()=>this.buttonClick()} ></Searchbutton>
-        <Searchtable  title={this.state.title}
-                    content={this.state.content}
-        ></Searchtable>
+        <Header/>
+        {/* <CallTable></CallTable> */}
       </div>
     )
   }

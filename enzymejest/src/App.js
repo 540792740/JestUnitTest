7bf8e7f68ca4ca16ba5adf90e16b2f7a6c90e114
540792740 <peterwangjiawei@gmail.com>
2021-07-08 20:07:04 -0400

    git

--- enzymejest/src/App.js
+++ enzymejest/src/App.js
@@ -7,7 +7,7 @@ import CounterList from "./components/Counter/CounterList";
 import ChartTest from "./components/chartJs/ChartTest";
 import User from "./components/User/User";
 import MyApp from './components/context/MyApp';
-import {AuthProvider} from './components/context/AuthProvider'
+import { AuthProvider } from './components/context/AuthProvider'
 
 const tempArr = [{
   fName: 'Joe',
@@ -18,9 +18,51 @@ const tempArr = [{
 }];
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+      showResult: 0
+    };
+  }
+  inputHandler(e) {
+    this.setState({ number: e.target.value })
+  }
+
+  primeNumberCheck() {
+    // 2, 3, 5, 7, 11
+    let number = this.state.number;
+    let showResult
+    for (let i = 2; i <= Math.sqat(number); i++) {
+      if (number % i === 0) {
+        console.log(number);
+        showResult = 'false';
+        break;
+      }
+    }
+
+    if (showResult !== 'false') showResult = 'true';
+    this.setState({
+      showResult: showResult
+    })
+
+
+  }
+
   render() {
     return (
       <div >
+        <div className='centerlize'>
+          <div>
+            <input value={this.state.number}
+              onChange={(e) => this.inputHandler(e)} /> enter your number
+            </div>
+          <div className='submitButton'>
+            <button onClick={() => this.primeNumberCheck()}>submit</button>
+          </div>
+          {this.state.showResult !== 0 && (this.state.number + '' + this.state.showResult)}
+        </div>
+
         {/* <Header/> */}
         {/* <CallTable></CallTable> */}
         {/* <section className="main">
@@ -29,9 +71,10 @@ export default class App extends Component {
         <CounterList />
         <User id={1}/> */}
         {/* <ParentContext /> */}
-        <ChartTest/>
+        {/* <ChartTest /> */}
       </div>
     )
   }
 }
+
 
